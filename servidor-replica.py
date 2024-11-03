81b672d8f619442c8b92649c0230c8eb5bd5bcd1
ArmandoVallejo <juanarmandovh20@gmail.com>
2024-11-03 13:40:47 -0600

    se añade el contenido de los 3 archivos principales

---
+++ servidor-replica.py
@@ -0,0 +1,18 @@
+
+import socket 
+ 
+def replica_server(): 
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    client.connect(('127.0.0.1', 9999))  # IP del servidor maestro 
+ 
+    while True: 
+        data = client.recv(1024) 
+        if not data: 
+            break 
+         
+        print(f"Datos replicados: {data.decode('utf-8')}") 
+ 
+    client.close() 
+ 
+if __name__ == "__main__": 
+    replica_server()
