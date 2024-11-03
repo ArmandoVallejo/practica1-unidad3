cbb501bc2e88124a529ac5bfe7633791a678a8d9
ArmandoVallejo <juanarmandovh20@gmail.com>
2024-11-03 14:00:26 -0600

    se modifica la replica para simular consistencia eventual

--- servidor-replica.py
+++ servidor-replica.py
@@ -1,18 +1,22 @@
+import socket
+import time
+import random
 
-import socket 
- 
-def replica_server(): 
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    client.connect(('127.0.0.1', 9999))  # IP del servidor maestro 
- 
-    while True: 
-        data = client.recv(1024) 
-        if not data: 
-            break 
-         
-        print(f"Datos replicados: {data.decode('utf-8')}") 
- 
-    client.close() 
- 
-if __name__ == "__main__": 
+def replica_server():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('127.0.0.1', 9999))  # IP del servidor maestro
+
+    while True:
+        # Simular latencia aleatoria
+        time.sleep(random.uniform(0.1, 1.0))  # Retraso aleatorio entre 100ms y 1s
+
+        data = client.recv(1024)
+        if not data:
+            break
+
+        print(f"Datos replicados (con retraso): {data.decode('utf-8')}")
+
+    client.close()
+
+if __name__ == "__main__":
     replica_server()
