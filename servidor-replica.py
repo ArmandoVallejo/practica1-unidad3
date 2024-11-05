d49468e72130adee1f7aa29d3f8a3b8a1650a3b4
ArmandoVallejo <juanarmandovh20@gmail.com>
2024-11-04 18:30:27 -0600

    se cambia la direccion ip a local en casa

--- servidor-replica.py
+++ servidor-replica.py
@@ -3,7 +3,7 @@ import socket
  
 def replica_server(): 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    client.connect(('127.0.0.1', 9999))  # IP del servidor maestro 
+    client.connect(('192.168.0.11', 9999))  # IP del servidor maestro 
  
     while True: 
         data = client.recv(1024) 
